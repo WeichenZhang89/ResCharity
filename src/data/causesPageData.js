e5444acffceac53fd5f993bdb20a143574f1737b
Weichen Zhang <eizhang@ucdavis.edu>
2024-11-15 01:49:46 -0800

    Update causesPageData.js

--- src/data/causesPageData.js
+++ src/data/causesPageData.js
@@ -14,8 +14,7 @@ const causesPageData = [
     image: "causes-page-img-2.jpg",
     category: "Medical",
     title: "Our donation is hope for poor childrens",
-    description:
-      "There are not many of passages of lorem ipsum avail isn alteration donationa in form simply free.",
+    description: "Get children food",
     raised: "25,270",
     goal: "30,000",
   },
@@ -24,8 +23,7 @@ const causesPageData = [
     image: "causes-page-img-3.jpg",
     category: "Medical",
     title: "Education for Poor Children",
-    description:
-      "There are not many of passages of lorem ipsum avail isn alteration donationa in form simply free.",
+    description: "Get children Education",
     raised: "25,270",
     goal: "30,000",
   },
