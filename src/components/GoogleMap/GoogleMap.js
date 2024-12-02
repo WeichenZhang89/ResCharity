93856b99cbe414debe63d38a89e86007c62ced2f
Sanchuan0209 <123540032+testme76@users.noreply.github.com>
2024-12-01 21:23:19 -0800

    google map update

--- src/components/GoogleMap/GoogleMap.js
+++ src/components/GoogleMap/GoogleMap.js
@@ -4,7 +4,7 @@ const GoogleMap = () => {
   return (
     <section className="contact-page-google-map">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4562.753041141002!2d-118.80123790098536!3d34.152323469614075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80e82469c2162619%3A0xba03efb7998eef6d!2sCostco+Wholesale!5e0!3m2!1sbn!2sbd!4v1562518641290!5m2!1sbn!2sbd"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3120.8547152339684!2d-121.75757992377977!3d38.53711707180318!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808529057369fccf%3A0x52f5f67bf92f29b0!2sKemper%20Hall!5e0!3m2!1sen!2sus!4v1733116771483!5m2!1sen!2sus"
         className="contact-page-google-map__one"
         allowFullScreen
       ></iframe>
